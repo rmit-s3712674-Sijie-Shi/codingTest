ac3b92c8ee3c567fdb4efa4767500101831832aa
rikusrocks <rikusrocks@gmail.com>
2023-05-04 19:52:29 +1000

    add get, update, delete issue end points

--- backend/src/functions.js
+++ backend/src/functions.js
@@ -1,17 +1,35 @@
 import { LocalData } from "./datastore/localData";
-import { Request, Response } from "express";
 import uuid from "uuid";
 
 const data = new LocalData()
 
 export const CreateIssue = (req, res) => {
-    console.log(req)
+    console.log(req);
     const { title, description } = req.body || {};
     const issue = {
         id: uuid(),
         title,
         description
-    }
-    data.create(issue)
-    res.status(201).json(issue)
+    };
+    data.create(issue);
+    res.status(201).json(issue);
+}
+
+export const GetIssue = (req, res) => {
+    const { id } = req.params;
+    const issue = data.read(id);
+    issue ? res.status(200).json(issue) : res.status(404).send('Issue not found');
+}
+
+export const UpdateIssue = (req, res) => {
+    const { id } = req.params;
+    const { title, description } = req.body;
+    const issue = data.update(id, { title, description });
+    issue ? res.status(200).json(issue) : res.status(404).send('Issue not found');
+}
+
+export const IssueDelete = (req, res) => {
+    const { id } = req.params;
+    data.delete(id);
+    res.status(204).send();
 }
