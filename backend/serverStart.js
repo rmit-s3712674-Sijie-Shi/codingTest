ff4871aeb922c3d3f21be48a5e0e2b43bf704de5
rikusrocks <rikusrocks@gmail.com>
2023-05-04 19:41:26 +1000

    start server function

---
+++ backend/serverStart.js
@@ -0,0 +1,19 @@
+import { Express } from "express";
+import { createServer } from "http";
+
+
+const serverStart = (app, port) => {
+    const server = createServer(app)
+
+    server.listen(8080, () => {
+        console.log(
+            `Server is running on port 8080`
+        )
+    })
+
+    server.on('error', (err) => {
+        console.error('Server starting error: ' + err)
+    })
+}
+
+export default serverStart;
